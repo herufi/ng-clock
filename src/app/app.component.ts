436c394c67f2bfe7d8735a2670524c1f594fde0d
Davis Okoth <davisokoth@gmail.com>
2022-06-21 13:58:46 +0300

    Angular stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CatsComponent } from './cats/cats.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-clock';
+
+  dateInfo = new Date();
+  hr = this.dateInfo.getHours() > 12 ? this.dateInfo.getHours() - 12 : this.dateInfo.getHours();
+  min = this.dateInfo.getMinutes();
+  sec = this.dateInfo.getSeconds();
+  milsec = this.dateInfo.getMilliseconds();
+
+  ngOnInit(): void {
+    this.setTime();
+  }
+
+  setTime() {
+    let hrAngle = this.hr * 30 + (this.min * 6 / 12);
+    let minAngle = this.min * 6 + (this.sec * 6 / 60);
+    let secAngle = this.sec * 6 + (this.milsec * 0.36 / 1000);
+
+    this.setAngle("hr-wrapper", hrAngle);
+    this.setAngle("min-wrapper", minAngle);
+    this.setAngle("sec-wrapper", secAngle);
+
+  }
+
+  setAngle(wrapper: any, angle: any) {
+    const divTag = document.querySelector("." + wrapper) as HTMLElement
+    if (divTag) {
+      divTag.style.transform = "rotate(" + angle + "deg)";
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatsComponent
   ],
   imports: [
     BrowserModule,
