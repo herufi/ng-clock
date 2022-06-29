256bcab7440baa42e95add6446fe37bba4fc54af
irenenjoki <irenewaweru9@gmail.com>
2022-06-29 14:32:16 +0300

    add navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatsComponent } from './cats/cats.component';
-
+import { NavigationBarComponent } from './navigation-bar/navigation-bar';
 @NgModule({
   declarations: [
     AppComponent,
-    CatsComponent
+    CatsComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
